102fc0449ce8eb4e966d8b830cee10cb27fadc80
Jeffrey Macks <jmacks1988@gmail.com>
2015-12-31 14:18:49 -0500

    deployment configuration:

--- my_page/server.js
+++ my_page/server.js
@@ -13,6 +13,6 @@ app.use(routes);
 
 
 //the server connection
-const server = app.listen(3000, function(){
+const server = app.listen(process.env.PORT || 3000, function(){
   console.log('JEFF MACKS HOMEPAGE RUNNING ON PORT 3000')
 });
